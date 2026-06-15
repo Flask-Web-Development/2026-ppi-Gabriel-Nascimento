b51b02d42322cdbf943998d62b3600220ec101e8
GGGabriell <165956690+GGGabriell@users.noreply.github.com>
2026-06-15 01:07:28 +0000

    feat: Blueprints and views

--- flask-tutorial/flaskr/__init__.py
+++ flask-tutorial/flaskr/__init__.py
@@ -22,7 +22,9 @@ def create_app(test_config=None):
         return 'Hello, World!'
 
     from . import db
-
     db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
 
     return app
